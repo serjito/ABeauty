57183cbe2730574f8fe1f6136a9fd6a635b7b214
sergio md <sergiomd@MBP-de-sergio.home>
2024-02-16 13:52:32 +0100

    footer

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,14 +1,4 @@
-'use client';
-import { useState } from 'react';
-import Contact from './Contact';
-import Button from './Button';
-
 function Footer() {
-  const [showContact, setShowContact] = useState(false);
-
-  const toggleContact = () => {
-    setShowContact(!showContact);
-  };
   return (
     <footer className="bottom-0 rounded-3xl shadow bg-gradient-to-t from-[#9958cc] to-[#6c71e5] backdrop-blur-lg w-[98%] mx-auto mb-10">
       <div className="w-full mx-autoflex flex-col rounded-3xl p-4 justify-center text-center text-xl bg-black/30">
@@ -26,11 +16,7 @@ function Footer() {
             </h3>
           </span>
         </span>
-        <div className="flex flex-col justify-center space-y-5 items-center mt-3 py-5 text-md font-medium sm:mt-0">
-          <Button onClick={toggleContact} />
-        </div>
       </div>
-      {showContact && <Contact onClose={toggleContact} />}
     </footer>
   );
 }
