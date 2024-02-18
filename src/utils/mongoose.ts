e0cca74d7891bd81d007b614e8c3113277167b6e
sergio md <sergiomd@MacBook-Pro-de-sergio.local>
2024-02-18 23:45:23 +0100

    changes mongo4

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -3,10 +3,10 @@ import mongoose, { connect, connection, ConnectionStates } from 'mongoose';
 mongoose.set('strictQuery', false);
 
 const connectDB = async () => {
-  if (!process.env.MONGO_URI) {
+  if (!process.env.MONGODB_URI) {
     throw new Error('MONGODB_KEY is not set in the environment variables');
   }
-  return await mongoose.connect(process.env.MONGO_URI);
+  return await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
